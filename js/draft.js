e3c451b8f43b11fa02deed7ab7d2493bb53564df
iscii <iscii@users.noreply.github.com>
2020-04-10 19:42:27 -0400

    draft
    i'm not exactly familiar with collaborative github yet so i dunno what i'm doing but i've put my thoughts on how the subject classes/objects should maybe look like in draft.js.
    
    Also, idk if i should be committing or creating pull requests. Pull requests might be smarter so we could talk through the changes, but I'll be committing this one since it's simply a rearrangement of files and a new draft file.

---
+++ js/draft.js
@@ -0,0 +1,44 @@
+/* AN EXTREMELY ROUGH DRAFT OF WHAT IT MAYBE WILLL LOOK LIKE. KEEP THAT IN MIND AS YOUR EYES BLEED READING IT
+function Subject()
+{
+    this.schedule = daily; //[monday, tuesday, wednesday, thursday, friday]
+    //daily, weekdays, weekends, alternating weedaays, alternating, custom
+
+    this.tasks = {
+        attendence = new attendence(this.schedule), //make this occur everyday the user has this subject (schedule)
+        homeworks = [], 
+        classworks = [],
+        projects = [],
+        test(s) = []
+    };
+}
+
+function Attendence(schedule, due)
+{
+    this.schedule = schedule; //this appears every time the schedule occurs
+    this.due = due; //some teachers require attendence to be filled by a certain time
+}
+function Homeworks(schedule, due) //input null for schedule to skip to 'due'
+{
+    this.schedule = schedule;//schedule -> for homeworks that are due daily, semi-daily, or on a regular basis
+    this.due = due;
+}
+function Classworks(schedule, due)
+{
+    this.schedule = schedule;
+    this.due = due;
+}
+function Projects(schedule, due)
+{
+    this.schedule = schedule;
+    this.due = due;
+    this.tasks = {
+        //custom user inputs for mini-tasks or to-dos -> is in an object cos we want it like a checkbox - user should be able to see which they've completed and which they haven't or smthn
+    };
+}
+function Tests(due)
+{
+    this.notes = [custom note1, custom note2];
+    this.due = due;
+}
+*/
